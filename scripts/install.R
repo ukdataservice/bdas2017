8077c6b28f966894bab63d37e7569cdd96947871
chris-ukds <chris.park@essex.ac.uk>
2017-07-12 15:23:13 +0100

    Create install.R

---
+++ scripts/install.R
@@ -0,0 +1,28 @@
+## Install required packages for the course.
+check_pkgs = function(pkgs) {
+  pkg_check = function(pkg) {
+    chooseCRANmirror(ind = 51)
+    pkg_name = paste0("package:", pkg)
+    pkg_inst = rownames(installed.packages())
+    if (pkg_name %in% search()) {
+      return()
+    }
+    if (!(pkg %in% pkg_inst)) {
+      install.packages(pkg, dep = TRUE)
+    }
+    if (load) {
+      library(pkg, char = TRUE, quietly = TRUE)
+    }
+  }
+  invisible(sapply(pkgs, pkg_check))
+  if (all(paste0("package:", pkgs) %in% search())) {
+    message("All the packages have been successfully loaded.")
+  }
+}
+
+pkgs = c("dplyr", "sparklyr", "readr", "tidyr", "rmarkdown",
+         "lubridate", "ggplot2", "leaflet", "hexbin",
+         "USAboundaries", "ggmap", "raster", "rgdal",
+         "profvis", "pryr", "microbenchmark")
+
+check_pkgs(pkgs)
